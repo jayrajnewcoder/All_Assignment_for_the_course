57b8c459d4efde62e561efe6202ddef202765daa
jayrajnewcoder <83805396+jayrajnewcoder@users.noreply.github.com>
2021-11-26 17:32:10 +0530

    Add files via upload

--- array.java
+++ array.java
@@ -1,38 +1,52 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
-import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		//Using single array for all three cases
-		Scanner sc = new Scanner(System.in);
-        int[] array = new int[10];
-        int size=10;
-        int sum= 0;
-        //Sum of all array elements
-        for (int i=0;i<10;i++){
-            array[i]=sc.nextInt();
-        }
-        for (int i:array){
-            sum=sum+i;
-        }
-        System.out.println("Sum of all array elements is "+sum);
-        //Find Middle element in array
-        int finalsize=Math.round(size/2);
-        int sans = array[finalsize-1];
-        System.out.println("Middle element in array is "+sans);
-        //Print all possitive number in array
-        System.out.println("All possitive element in array are=");
-        for (int i=0;i<10;i++){
-            if (array[i]>0){
-                System.out.println(array[i]);
+public class array{ 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //add two 2-D matrices
+        System.out.print("Enter Number of rows=");//Number of rows are same for other 2-D array also
+        int rows = sc.nextInt();
+        System.out.print("Enter Number of column=");//Number of column will be same for other 2-D array also
+        int column = sc.nextInt();
+        int farr[][] = new int[rows][column];
+        int sarr[][] = new int[rows][column];
+        int finalarray[][] = new int[rows][column];
+        System.out.println("Enter elements for first array=");
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<column;j++){
+                farr[i][j]=sc.nextInt();
             }
         }
-        
-	}
+        System.out.println("Enter elements for Second array=");
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<column;j++){
+                sarr[i][j]=sc.nextInt();
+            }
+        }
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<column;j++){
+                finalarray[i][j]=farr[i][j]+sarr[i][j];
+            }
+        }
+        System.out.println("After addition of two array matrix will be");
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<column;j++){
+                System.out.print(finalarray[i][j]+" ");
+            }
+            System.out.println();
+        }
+        //Print Element Columnwise
+        int columnw[][] = new int[rows][column];
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<column;j++){
+                columnw[i][j]=sc.nextInt();
+            }
+        }
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<column;j++){
+                System.out.print(columnw[i][j]+" ");
+            }
+        }        
+
+    }
 }
